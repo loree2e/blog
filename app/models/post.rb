79f3981631e01ce3b409413d899ab1ea2cfba991
Kurt Robert Rudolph <rudolph.kurt@rudyindustries.com>
2013-05-15 18:14:39 -0500

    I think this is what you're looking for...

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,6 +10,8 @@ class Post < ActiveRecord::Base
   validates :name, presence: true, length: { maximum: 40 }
   validates :title, presence: true, length: { maximum: 40 }
 
+  scope :order_by_author, lambda{ joins(:author).order( 'authors.name') }
+
   def hello
   	"Hello #{self.name}. Thank you for this post."
   end
